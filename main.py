2f07264d03b791e08ab21011fb46079c3ec79e63
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2019-03-28 01:14:51 -0300

    working for primary order logic --parser incomplete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
-from operations import AND, IMPLICA, NOT, OR
-from parser import Atom
+# -*- coding: utf-8 -*- 
+from parser import Atom, is_valid, run
 
 if __name__ == "__main__":
 
@@ -9,5 +8,18 @@ if __name__ == "__main__":
     A = Atom(True)
     B = Atom(False)
 
+    states_dict= {
+        'p': {'A':Atom(True), 'B': Atom(False), 'C': Atom(True)},
+        'q': {'A':Atom(False), 'B': Atom(False), 'C': Atom(True)}
+    }
 
-    print(IMPLICA(B,AND(A,B)))
+    neighbours_list = {
+        'p': ['q']
+    } 
+
+
+
+    is_valid('p-A', states_dict)
+    print(run('p-!AvB->B', states_dict))
+
+    #print(IMPLICA(B,AND(A,B)))
